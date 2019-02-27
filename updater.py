a62050267868b48643a71755e980288c3b4c91e8
lukshan13 <lukshan@hotmail.co.uk>
2019-02-27 12:10:52 +0000

    updater

--- updater.py
+++ updater.py
@@ -4,6 +4,8 @@ def run():
         print("\nchecking for updates")
         from urllib.request import urlopen
         import os
+
+        
         try:
                 content = urlopen("https://raw.githubusercontent.com"+URL+"/master/Main/version.txt").read().decode()
         except:
@@ -27,15 +29,30 @@ def run():
                 print ("Update available, program will be updated\n")
                 update()
 def update():
+
+
+        db = open("main/ATS/ats.db", 'r+b')
+        config = open("main/ATS/static/config.json", 'r+b')
+        sk = open("main/ATS/static/sk.txt", 'r+b')
+
         from urllib.request import urlretrieve
         import zipfile
+
         urlretrieve("https://github.com"+URL+"/archive/master.zip", "download.zip")
         with zipfile.ZipFile("download.zip","r") as zip_ref:
-                    zip_ref.extractall("../")
+                zip_ref.extractall("../")
         import os, shutil
         os.remove("download.zip")
-        
+        with open("main/ATS/ats.db", 'wb') as db1:
+                db1.write(db.read())
+        with open("main/ATS/static/config.json", 'wb') as config1:
+                config1.write(config.read())
+        with open("main/ATS/static/sk.txt", 'wb') as sk1:
+                sk1.write(sk.read())
+        db.close()
+        config.close()
+        sk.close()
 
 
-run()
-
+#run()
+update()
